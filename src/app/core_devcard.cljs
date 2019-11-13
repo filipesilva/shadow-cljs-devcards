6a09329818ec4390493088ff5f05acf0188a95b6
Filipe Silva <filipematossilva@gmail.com>
2019-11-13 19:01:05 +0000

    use devcards/reagent

--- src/app/core_devcard.cljs
+++ src/app/core_devcard.cljs
@@ -5,6 +5,8 @@
    [reagent.core :as r :include-macros true]
    [devcards.core :as devcards :include-macros true :refer [defcard]]))
 
+(def bmi-data (r/atom {:height 180 :weight 80}))
+
 (defn calc-bmi [bmi-data]
   (let [{:keys [height weight bmi] :as data} bmi-data
         h (/ height 100)]
@@ -13,12 +15,12 @@
       (assoc data :weight (* bmi h h)))))
 
 (defn slider [bmi-data param value min max]
-   [:input {:type "range" :value value :min min :max max
-            :style {:width "100%"}
-            :on-change (fn [e]
-                         (swap! bmi-data assoc param (.-target.value e))
-                         (when (not= param :bmi)
-                           (swap! bmi-data assoc :bmi nil)))}])
+  [:input {:type "range" :value value :min min :max max
+           :style {:width "100%"}
+           :on-change (fn [e]
+                        (swap! bmi-data assoc param (.. e -target -value))
+                        (when (not= param :bmi)
+                          (swap! bmi-data assoc :bmi nil)))}])
 
 (defn bmi-component [bmi-data]
   (let [{:keys [weight height bmi]} (calc-bmi @bmi-data)
@@ -27,23 +29,23 @@
                            (< bmi 25) ["inherit" "normal"]
                            (< bmi 30) ["orange" "overweight"]
                            :else ["red" "obese"])]
+    [:div
+     [:h3 "BMI calculator"]
      [:div
-      [:h3 "BMI calculator"]
-      [:div
-       [:span (str "Height: " (int height) "cm")]
-       (slider bmi-data :height height 100 220)]
-      [:div
-       [:span (str "Weight: " (int weight) "kg")]
-       (slider bmi-data :weight weight 30 150)]
-      [:div
-       [:span (str "BMI: " (int bmi) " ")]
-       [:span {:style {:color color}} diagnose]
-       (slider bmi-data :bmi bmi 10 50)]]))
+      "Height: " (int height) "cm"
+      [slider bmi-data :height height 100 220]]
+     [:div
+      "Weight: " (int weight) "kg"
+      [slider bmi-data :weight weight 30 150]]
+     [:div
+      "BMI: " (int bmi) " "
+      [:span {:style {:color color}} diagnose]
+      [slider bmi-data :bmi bmi 10 50]]]))
 
 (defcard bmi-calculator
   "*Code taken from the Reagent readme.*"
-  (fn [data-atom _] (r/as-element (bmi-component data-atom)))
-  {:height 180 :weight 80}
+  (devcards/reagent bmi-component)
+  bmi-data
   {:inspect-data true
    :frame true
    :history true})
